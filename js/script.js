75b03305d5212d2da95bfb2f1ec42176a74b10ac
Justin O'Reilly <joreilly25@gmail.com>
2021-05-01 15:43:21 -0400

    practive with events

--- js/script.js
+++ js/script.js
@@ -31,3 +31,47 @@ container.appendChild(content);
 content.appendChild(redPara);
 content.appendChild(blueHead3);
 container.appendChild(blackAndPink);
+
+//Button alert
+const btnAlso = document.querySelector("#btnAlso");
+btnAlso.onclick = () => alert("Hello World");
+
+//Another alert type
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  alert("Hello World");
+});
+
+// METHOD 1
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+
+// METHOD 2
+// btn.onclick = alertFunction;
+const clickMe = document.querySelector("#clickMe");
+// METHOD 3
+clickMe.addEventListener("click", alertFunction);
+
+// btn.addEventListener('click', function (e) {
+//   console.log(e);
+// });
+
+// btn.addEventListener('click', function (e) {
+//   console.log(e.target);
+// });
+
+// btn.addEventListener('click', function (e) {
+//   e.target.style.background = 'blue';
+// });
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
